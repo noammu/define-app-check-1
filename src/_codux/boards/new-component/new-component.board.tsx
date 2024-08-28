7920f3332c02ae6e333bf3c8d2e263d6759f62d4
Noam Musba <noammu@wix.com>
2024-08-28 12:34:47 +0300

    xxx

---
+++ src/_codux/boards/new-component/new-component.board.tsx
@@ -0,0 +1,18 @@
+import { NewComponent } from '../../../components/new-component/new-component';
+import { ContentSlot, createBoard } from '@wixc3/react-board';
+import { ComponentWrapper } from '/src/_codux/board-wrappers/component-wrapper';
+
+export default createBoard({
+    name: 'NewComponent',
+    Board: () => (
+        <ComponentWrapper settings={{}}>
+            <ContentSlot>
+                <NewComponent />
+            </ContentSlot>
+        </ComponentWrapper>
+    ),
+    isSnippet: true,
+    environmentProps: {
+        canvasMargin: { right: 0, bottom: 0, left: 0, top: 0 },
+    },
+});
